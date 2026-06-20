14bfb3f6b4223d88505b6ec5d2a9d97ffd39a24a
Hazim Alqralh <alqralh2003@gmail.com>
2026-06-21 01:28:43 +0300

    fix sequelize instance issue

--- delivery-accounting-backend/src/config/database.js
+++ delivery-accounting-backend/src/config/database.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL,
+  {
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
+  }
+);
+
+module.exports = sequelize;
