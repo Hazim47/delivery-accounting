aba35c8c33337ce6f129e3fa9a177265a489f88d
Hazim Alqralh <alqralh2003@gmail.com>
2026-06-23 22:29:36 +0300

    deploy

--- delivery-accounting-backend/src/routes/importRoutes.js
+++ delivery-accounting-backend/src/routes/importRoutes.js
@@ -11,7 +11,11 @@ const {
   getImportLogs,
   deleteImportLog,
 } = require("../controllers/importLogController");
+const fs = require("fs");
 
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads", { recursive: true });
+}
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
